d070a720aff0dc7334e86d7dc3af084944d28b0b
renato_O <renatorsdeoliveira@gmail.com>
2020-10-13 21:11:13 -0300

    Configurado o porm e criado a classe principal

---
+++ src/main/java/br/edu/fasam/tcc/renato/SpringBootApplicationRenato.java
@@ -0,0 +1,11 @@
+package br.edu.fasam.tcc.renato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationRenato {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplicationRenato.class, args);
+    }
+}
