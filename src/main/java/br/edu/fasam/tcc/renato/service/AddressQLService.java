0a1558ebf7a20506985714622e2e9f80ed7b9286
renato_O <renatorsdeoliveira>
2020-12-04 21:09:43 -0300

    Finalizado a classe Geo

--- src/main/java/br/edu/fasam/tcc/renato/service/AddressQLService.java
+++ src/main/java/br/edu/fasam/tcc/renato/service/AddressQLService.java
@@ -1,7 +1,9 @@
 package br.edu.fasam.tcc.renato.service;
 
 import br.edu.fasam.tcc.renato.model.AddressQL;
+import br.edu.fasam.tcc.renato.model.GeoQL;
 import br.edu.fasam.tcc.renato.repository.AddressQLRepository;
+import br.edu.fasam.tcc.renato.repository.GeoQLRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +13,14 @@ import java.util.Optional;
 public class AddressQLService {
 
     @Autowired
+    private GeoQLService geoQLService;
+
+    @Autowired
     private AddressQLRepository addressQLRepository;
 
-    public AddressQL create(AddressQL address) {
-        return addressQLRepository.save(address);
+    public AddressQL create(AddressQL addressQL, Integer id){
+        addressQL.setGeo(geoQLService.findGeoQLById(id).get());
+        return addressQLRepository.save(addressQL);
     }
 
     public void deleteDoRenato(AddressQL address){
