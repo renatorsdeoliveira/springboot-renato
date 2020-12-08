c7e69fe84a539037367d0d31e37cb4ebfecb2944
renato_O <renatorsdeoliveira>
2020-12-08 07:23:53 -0300

    Corrigido service e repository do Todo

--- src/main/java/br/edu/fasam/tcc/renato/graphql/TodoGraphQL.java
+++ src/main/java/br/edu/fasam/tcc/renato/graphql/TodoGraphQL.java
@@ -1,28 +1,27 @@
 package br.edu.fasam.tcc.renato.graphql;
 
-import br.edu.fasam.tcc.renato.model.TodoQL;
-import br.edu.fasam.tcc.renato.service.TodoQLService;
+import br.edu.fasam.tcc.renato.model.Todo;
+import br.edu.fasam.tcc.renato.service.TodoService;
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
-import java.util.Optional;
 
 @Component
 public class TodoGraphQL implements GraphQLMutationResolver, GraphQLQueryResolver {
 
     @Autowired
-    private TodoQLService todoQLService;
+    private TodoService todoService;
 
-    public Optional<TodoQL> findTodo(Integer id){
-        return todoQLService.findTodoQLById(id);
+    public Todo findTodo(Integer id){
+        return todoService.read(id);
     }
 
     @Transactional(rollbackOn = Exception.class)
-    public TodoQL createTodo(TodoQL todo) {
-        return todoQLService.create(todo);
+    public Todo createTodo(Todo todo) {
+        return todoService.create(todo);
     }
 
 }
